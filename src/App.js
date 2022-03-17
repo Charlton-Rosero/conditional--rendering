f94210b459a4f4b19c95fe6fb4a81cd05c4479b0
Charlton_Rosero <charltonprosero@gmail.com>
2022-03-17 15:29:50 -0400

    Conditional rendering

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import LoginControl from './components/LoginControl';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginControl />
+     
+    </div>
+  );
+}
+
+export default App;
+
